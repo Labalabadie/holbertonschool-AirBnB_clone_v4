19e7b08d9b7e651ed8e49e88f2bca6649991e5f8
MauriPercovich <mauriciopercovich22@gmail.com>
2022-09-17 19:23:45 +0000

    js script

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,10 @@
+    document.addEventListener("DOMContentLoaded", function () {
+        let dicc = {};
+        checkbox.addEventListener('change', function() {
+	    if (this.checked) {
+		dicc[]
+            } else {
+		console.log("Checkbox is not checked..");
+	}
+    });
+});
