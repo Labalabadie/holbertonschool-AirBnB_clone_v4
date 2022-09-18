fb1697a16d56991c8d04184972ec95aa5600e25d
MauriPercovich <mauriciopercovich22@gmail.com>
2022-09-18 01:02:21 +0000

    script

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,10 +1,15 @@
-    document.addEventListener("DOMContentLoaded", function () {
-        let dicc = {};
-        checkbox.addEventListener('change', function() {
-	    if (this.checked) {
-		dicc[]
-            } else {
-		console.log("Checkbox is not checked..");
-	}
-    });
+$(document).ready(function () {
+  const dic = {};
+  const $amenitieschecker = $('input[type=checkbox]');
+  const $checkedamenities = $('div.amenities h4');
+
+  $amenitieschecker.click(function () {
+    if ($(this).is(':checked')) {
+	    dic[$(this).data('id')] = $(this).data('name');
+	    $checkedamenities.text(Object.values(dic).join(', '));
+    } else if ($(this).is(':not(:checked)')) {
+	    delete dic[$(this).data('id')];
+	    $checkedamenities.text(Object.values(dic).join(', '));
+    }
+  });
 });
